722cdfbc301da20d01e03d2c8804cda12cff9845
Rachael <rachael.rocha@mavs.uta.edu>
2023-11-20 18:02:59 -0600

    made it look pretty

--- petconnect/navigation/screens/LoginScreen.js
+++ petconnect/navigation/screens/LoginScreen.js
@@ -1,7 +1,11 @@
 import { View, Text, StyleSheet, KeyboardAvoidingView, TextInput, TouchableOpacity } from 'react-native'
-import React from 'react'
+import React, { useState } from 'react'
 
 const LoginScreen = () => {
+    const [email, setEmail] = useState('')
+    const [password,setPassword] = useState('')
+
+
   return (
    <KeyboardAvoidingView
       style={styles.container} 
@@ -51,28 +55,48 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center',
   },
-  container: {
-
-  },
   inputcontainer: {
-
+    width:'80%',
   },
   input: {
-
+    backgroundColor: 'white',
+    paddingHorizontal: 15,
+    paddingVertical: 10,
+    borderRadius: 10,
+    marginTop: 5,
   },
   buttonContainer: {
-
+    width: '50%',
+    justifyContent: 'center',
+    alignItems:'center',
+    marginTop: 40,
   },
   button: {
+    backgroundColor: '#0782F9',
+    width: '100%',
+    padding: 15,
+    borderRadius: 10,
+    alignItems: 'center',
 
   },
   buttonText: {
+    color: 'white',
+    fontWeight: 700,
+    fontSize: 16,
 
   },
   buttonOutline: {
+    backgroundColor: 'white',
+    marginTop: 5,
+    borderColor: '#0782F9',
+    borderWidth: 2,
+
     
   },
   buttonOutlineText: {
+    color: '#0782F9',
+    fontWeight: 700,
+    fontSize: 16,
 
   },
 })
