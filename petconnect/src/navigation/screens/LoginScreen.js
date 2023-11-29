c8369eb28b320cf14b45d415f5418508978fd15d
Nathan Chugito <nchugito@gmail.com>
2023-11-28 22:00:09 -0600

    branch 2 test

--- petconnect/src/navigation/screens/LoginScreen.js
+++ petconnect/src/navigation/screens/LoginScreen.js
@@ -1,99 +1,104 @@
-import { View, Text, StyleSheet, KeyboardAvoidingView, TextInput, TouchableOpacity } from 'react-native'
-import React, { useState, useEffect } from 'react'
-import {auth} from '../../../firebase.ignore.js'
-import { useNavigation } from '@react-navigation/native'
+import { View, Text, StyleSheet, KeyboardAvoidingView, TextInput, TouchableOpacity, Image } from 'react-native';
+import React, { useState } from 'react';
+import { auth } from '../../../firebase.ignore.js';
+import { useNavigation } from '@react-navigation/native';
 
 const LoginScreen = () => {
-    const [email, setEmail] = useState('')
-    const [password,setPassword] = useState('')
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
 
-    const navigation = useNavigation()
+  const navigation = useNavigation();
 
-    const handleSignUp = () => {
-      auth
-        .createUserWithEmailAndPassword(email,password)
-        .then(userCredentials => {
-          const user = userCredentials.user;
-          console.log('Registered ' + user.email);
-        })
-        .catch(error => alert(error.message))
-    }
+  const handleSignUp = () => {
+    auth
+      .createUserWithEmailAndPassword(email, password)
+      .then((userCredentials) => {
+        const user = userCredentials.user;
+        console.log('Registered ' + user.email);
+      })
+      .catch((error) => alert(error.message));
+  };
 
-
-    const handleLogin = () => {
-      auth
-      .signInWithEmailAndPassword(email,password)
-      .then(userCredentials => {
-          const user = userCredentials.user;
-          console.log('Logged in with: ' + user.email);
-        })
-        .catch(error => alert(error.message))
-    }
+  const handleLogin = () => {
+    auth
+      .signInWithEmailAndPassword(email, password)
+      .then((userCredentials) => {
+        const user = userCredentials.user;
+        console.log('Logged in with: ' + user.email);
+      })
+      .catch((error) => alert(error.message));
+  };
 
   return (
-   <KeyboardAvoidingView
-      style={styles.container} 
-      behavior='padding' >
-    <View 
-      style = {styles.inputcontainer}>
-      <TextInput 
-        placeholder='Email' 
-        value={email} 
-        onChangeText={text => setEmail(text) } 
-        style = {styles.input} 
-        placeholderTextColor="#000">
+    <KeyboardAvoidingView style={styles.container} behavior="padding">
+      <View style={styles.logoContainer}>
+        {/* Add your logo here */}
+        <Image source={require('./images/edit.png')} style={styles.logo} />
+      </View>
+      <View style={styles.inputcontainer}>
+        <TextInput
+          placeholder="Email"
+          value={email}
+          onChangeText={(text) => setEmail(text)}
+          style={styles.input}
+          placeholderTextColor="#000"
+        />
+        <TextInput
+          placeholder="Password"
+          value={password}
+          onChangeText={(text) => setPassword(text)}
+          style={styles.input}
+          secureTextEntry
+          placeholderTextColor="#000"
+        />
+        <View style={styles.line} />
+      </View>
+      <View style={styles.buttonContainer}>
+        <TouchableOpacity onPress={handleLogin} style={styles.button}>
+          <Text style={styles.buttonText}>Login</Text>
+        </TouchableOpacity>
+        <TouchableOpacity onPress={handleSignUp} style={[styles.button, styles.buttonOutline]}>
+          <Text style={styles.buttonOutlineText}>Register</Text>
+        </TouchableOpacity>
+      </View>
+    </KeyboardAvoidingView>
+  );
+};
 
-      </TextInput>
-      <TextInput 
-        placeholder='Password' 
-        value={password} 
-        onChangeText={text => setPassword(text)} 
-        style = {styles.input} 
-        secureTextEntry 
-        placeholderTextColor="#000">
-      </TextInput>
-    </View>
-    <View style = {styles.buttonContainer}>
-      <TouchableOpacity
-        onPress={handleLogin}
-        style={styles.button}
-      >
-       <Text style={styles.buttonText}>Login</Text> 
-      </TouchableOpacity>
-      <TouchableOpacity
-        onPress={handleSignUp}
-        style={[styles.button, styles.buttonOutline]}
-      >
-       <Text style={[styles.buttonOutlineText]}>Register</Text> 
-      </TouchableOpacity>
-    </View>
-   </KeyboardAvoidingView>
-  )
-}
-
-export default LoginScreen
+export default LoginScreen;
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
+    backgroundColor: '#fff',
+  },
+  logoContainer: {
+    marginBottom: 1, // Adjust as needed
+  },
+  logo: {
+    width: 300, // Adjust the width of your logo
+    height: 75, // Adjust the height of your logo
+    resizeMode: 'contain', // Adjust the resizeMode as needed
   },
   inputcontainer: {
-    width:'80%',
+    width: '80%',
   },
   input: {
+    borderWidth: 1, // Add this line to set the border width
+    borderColor: '#333333', // Add this line to set the border color
     backgroundColor: 'white',
     paddingHorizontal: 15,
     paddingVertical: 10,
     borderRadius: 10,
-    marginTop: 5,
+    marginTop: 10,
   },
   buttonContainer: {
-    width: '50%',
+    width: '80%',
     justifyContent: 'center',
-    alignItems:'center',
-    marginTop: 40,
+    alignItems: 'center',
+    marginTop: 1,
   },
   button: {
     backgroundColor: '#0782F9',
@@ -101,26 +106,26 @@ const styles = StyleSheet.create({
     padding: 15,
     borderRadius: 10,
     alignItems: 'center',
-
+    
   },
   buttonText: {
     color: 'white',
-    fontWeight: 700,
     fontSize: 16,
-
   },
   buttonOutline: {
     backgroundColor: 'white',
-    marginTop: 5,
+    marginTop: 10,
     borderColor: '#0782F9',
     borderWidth: 2,
-
-    
   },
   buttonOutlineText: {
     color: '#0782F9',
-    fontWeight: 700,
     fontSize: 16,
-
   },
-})
+  line: {
+    height: 1,
+    backgroundColor: '#333333',
+    width: '100%',
+    marginVertical: 10, // Adjust the vertical margin as needed
+  },
+});
